f93cb16e7df371645f2991e05347369b2a54b6a3
Fredrik <fredrik.k.andersson@gmail.com>
2019-12-31 23:18:14 +0100

    Stop growth by setting blockData.Stage to '1'.
    Since the previous version did not clear maxHeight from the
    base block (of the plant) it survived if the bamboo
    was completly removed and replanted. Changing the state
    value of the blockData is more inline how mincraft works.
    
    The topmost block will also be set to have large leaves.
    
    Known issues:
    - a placeItem action with bone meal might throw an error
      due to that this event is run async and is not synced
      with the spread evend we're handling.

--- src/main/java/com/yellowfortyfour/spigot/randomgrowth/BambooGrowLimmiter.java
+++ src/main/java/com/yellowfortyfour/spigot/randomgrowth/BambooGrowLimmiter.java
@@ -6,69 +6,64 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.block.data.*;
+import org.bukkit.block.data.type.Bamboo;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.block.Block;
 
 public class BambooGrowLimmiter implements Listener {
   final Plugin plugin;
+  final Random random = new Random();
 
-  public BambooGrowLimmiter(Plugin pl) {
+
+  public BambooGrowLimmiter(Plugin plugin) {
     Bukkit.getLogger().info("[BambooGrowLimmiter] watching");
 
-    plugin = pl;
+    this.plugin = plugin;
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.HIGHEST)
   public void onBlockSpreadEvent(BlockSpreadEvent event) {
-
-    Block sourceBlock = event.getSource();
-    Block newBlock = event.getBlock();
-    Block rootBlock = null;
-
+    final Block sourceBlock = event.getSource();
+    
     if (sourceBlock.getType() == Material.BAMBOO) {
-
+      final Block newBlock = event.getBlock();
       final Location sourceLoc = sourceBlock.getLocation();
       final Location newLoc = newBlock.getLocation();
+      final World world = sourceBlock.getWorld();
       Location rootLoc = newLoc.clone();
-
-      World world = sourceBlock.getWorld();
+      
       for (int y = sourceLoc.getBlockY(); y > 0; y--) {
         if (world.getBlockAt(sourceLoc.getBlockX(), y, sourceLoc.getBlockZ()).getType() != sourceBlock.getType()) {
           rootLoc.setY(++y);
           break;
         }
       }
-      int plantLenght = newLoc.getBlockY() - rootLoc.getBlockY() + 1; // length incl. new block that is about to grow
-      rootBlock = world.getBlockAt(rootLoc);
+      int plantLenght = newLoc.getBlockY() - rootLoc.getBlockY() + 1;
 
-      if (plantLenght > getMaxHeight(rootBlock)) {
-        org.bukkit.block.data.type.Bamboo bambooBlockData = (org.bukkit.block.data.type.Bamboo) sourceBlock.getBlockData();
-        bambooBlockData.setStage(1);
-        sourceBlock.setBlockData(bambooBlockData);
+      if (plantLenght > getMaxHeight() && !allowGrowth(plantLenght)) {
+        org.bukkit.block.data.type.Bamboo bambooData = (org.bukkit.block.data.type.Bamboo) sourceBlock.getBlockData();
+        bambooData.setStage(1);
+        bambooData.setLeaves(Bamboo.Leaves.LARGE);
+        sourceBlock.setBlockData(bambooData);
 
         event.setCancelled(true);
       }
     }
   }
 
-  private int getMaxHeight(Block rootBlock) {
-    int maxHeight = 0;
-    if (rootBlock.hasMetadata("maxHeight")) {
-      maxHeight = rootBlock.getMetadata("maxHeight").get(0).asInt();
-    } else {
-      maxHeight = getRandom(3, 25);
-      rootBlock.setMetadata("maxHeight", new FixedMetadataValue(plugin, maxHeight));
-    }
-    return maxHeight;
+  private Boolean allowGrowth(int plantHeight) {
+    final float factor = 1.0f - (plantHeight / 48.0f);
+    final float ran = random.nextFloat();
+    final Boolean allowed = factor > ran;
+    return allowed;
   }
 
-  private int getRandom(int min, int max) {
-    assert (min < max);
-
-    Random random = new Random();
-    return random.nextInt((max - min) + 1) + min;
+  private int getMaxHeight() {
+    return 3;
   }
 }
